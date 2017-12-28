edf4c64764f43420192ab778afeaf0bfb174ba36
Rafael Carlos <shanglangjunior@hotmail.com>
2017-12-28 16:46:32 -0300

    Refazendo ORM de entidades do banco.

---
+++ src/main/java/com/rafael/easygasws/repositorios/CartaoRepository.java
@@ -0,0 +1,12 @@
+package com.rafael.easygasws.repositorios;
+
+import com.rafael.easygasws.entidades.Cartao;
+
+/**
+ *
+ * @author Rafael Carlos Oliveira <rafaellcarloss@hotmail.com>
+ * @date 28/12/2017
+ */
+public class CartaoRepository extends RepositorioGenerico<Integer, Cartao> {
+    
+}
